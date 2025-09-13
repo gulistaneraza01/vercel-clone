2e429c79de2afeda859ced98c12a32f73a71eb8b
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-13 23:35:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use("/", (req, res) => {
+  res.json({ status: "status from server " });
+});
+
+app.listen(port, () => {
+  console.log(`Server Listening On PORT: ${port}`);
+});
