09582a2869a80c2ab27eeecc8e0adc7a3b1ea479
Gulistane Raza <gulistaneraza01@gmail.com>
2025-10-04 23:31:58 +0530

    isdeploy api

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,23 @@
 import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
 import express from "express";
 import { nanoid } from "nanoid";
+import dotenv from "dotenv";
+import { GetObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
+import s3client from "./config/s3client.js";
+
+dotenv.config();
 
 const app = express();
-
 const port = 8001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const ecsClient = new ECSClient({
-  region: "ap-south-1",
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: "",
-    secretAccessKey: "",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
@@ -19,37 +26,53 @@ app.post("/newproject", async (req, res) => {
   const projectId = nanoid(5);
 
   //ecs setup
-  const command = new RunTaskCommand({
-    cluster: "",
-    taskDefinition: "",
-    launchType: "FARGATE",
-    count: 1,
-    networkConfiguration: {
-      awsvpcConfiguration: {
-        assignPublicIp: "ENABLED",
-        subnets: ["", "", ""],
-        securityGroups: [""],
-      },
-    },
-    overrides: {
-      containerOverrides: [
-        {
-          name: "",
-          environment: [
-            { name: "GIT_REPOSITORY__URL", value: githubURL },
-            { name: "PROJECT_ID", value: projectId },
-          ],
-        },
-      ],
-    },
-  });
+  // const command = new RunTaskCommand({
+  //   cluster: "",
+  //   taskDefinition: "",
+  //   launchType: "FARGATE",
+  //   count: 1,
+  //   networkConfiguration: {
+  //     awsvpcConfiguration: {
+  //       assignPublicIp: "ENABLED",
+  //       subnets: ["", "", ""],
+  //       securityGroups: [""],
+  //     },
+  //   },
+  //   overrides: {
+  //     containerOverrides: [
+  //       {
+  //         name: "",
+  //         environment: [
+  //           { name: "GIT_REPOSITORY__URL", value: githubURL },
+  //           { name: "PROJECT_ID", value: projectId },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  // });
 
-  await ecsClient.send(command);
+  // await ecsClient.send(command);
 
   res.json({
     status: "queue",
-    data: { projectId, url: `http://${projectId}:8000/` },
+    data: { projectId, url: `http://${projectId}.localhost:8000/` },
   });
+});
+
+app.get("/isdeploy", async (req, res) => {
+  try {
+    const { projectId } = req.body;
+
+    const command = new HeadObjectCommand({
+      Bucket: "vercel-demo-clone",
+      Key: `output/${projectId}/index.html`,
+    });
+    const data = await s3client.send(command);
+
+    return res.json({ status: "done", data });
+  } catch (error) {
+    res.status(400).json({ status: "error", err: error.message });
+  }
 });
 
 app.listen(port, () => {
